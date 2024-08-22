02b0a84dcb72d55faa3cc8f3f2f0d7103804e4f0
VictorCodinhoto <133376215+VictorCodinhoto@users.noreply.github.com>
2024-08-22 11:17:19 -0300

    Começo jogo pedra,papel,tesoura

--- jogo/bin/jogo.dart
+++ jogo/bin/jogo.dart
@@ -1,5 +1,21 @@
 import 'package:jogo/jogo.dart' as jogo;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${jogo.calculate()}!');
+  String numString;
+  String 
+  do {
+    print("Digite 1 Para começar uma rodada,2 para acabar a partida e encerrar: ");
+    numString = stdin.readLineSync()!;
+    if (numString != null) {
+      int escolha = int.parse(numString);
+      if (escolha == 1 || escolha == 2) {
+        if (escolha == 2) {
+          break;
+        }
+        print("Escolha 1 para pedra, 2 para papel e 3 para tesoura");
+
+      }
+    }
+  } while (condition);
 }
--- jogo/lib/jogo.dart
+++ jogo/lib/jogo.dart
@@ -1,3 +1,12 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+import 'dart:js_util';
+import 'dart:math';
+
+int escolhaMaquina() {
+  Random r = newObject();
+  int num = 0;
+  while (num == 0) {
+    num = r.nextInt(3);
+  }
+  return num;
 }
